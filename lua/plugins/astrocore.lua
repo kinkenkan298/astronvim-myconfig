3539458f4b6692bd7cd190b3e3f07f8bd41a8f03
Akin <41711810+kinkenkan298@users.noreply.github.com>
2024-12-14 14:14:16 +0700

    ubah dikit

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -32,6 +32,11 @@ return {
         spell = false, -- sets vim.opt.spell
         signcolumn = "yes", -- sets vim.opt.signcolumn to yes
         wrap = false, -- sets vim.opt.wrap
+        conceallevel = 0, -- disable conceal
+        linebreak = true, -- linebreak soft wrap at words
+        list = true, -- show whitespace characters
+        showbreak = "﬌ ",
+        listchars = { tab = " ", extends = "⟩", precedes = "⟨", trail = "·", nbsp = "␣" },
       },
       g = { -- vim.g.<key>
         -- configure global vim variables (vim.g)
